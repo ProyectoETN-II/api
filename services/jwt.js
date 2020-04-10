86ea8ad89efaba25c256d2d82698e44419e4ae80
Àngel <angelcarballo66666@gmail.com>
2020-04-10 16:31:27 -0300

    changin the correct secret

--- services/jwt.js
+++ services/jwt.js
@@ -2,7 +2,7 @@
 
 var jwt = require('jwt-simple');
 var moment = require('moment');
-var secret = 'desarrollandoproyecto2adjkgw';
+var secret = 'clave_secreta_proyecto';
 
 exports.createToken = function(user){
 	var payload = {
